aee785e0c56e82c308884b0a2df0b5127a6d5b66
Shahar Yakir <shahar.yakir@gmail.com>
2022-10-30 16:39:01 +0200

    with deploy

--- build/registry.deploy.ts
+++ build/registry.deploy.ts
@@ -1,66 +1,90 @@
 import {
-    Address,
-    CellMessage,
-    CommonMessageInfo,
-    InternalMessage,
-    SendMode,
-    toNano,
-    TupleSlice,
-    WalletContract
+  Address,
+  CellMessage,
+  CommonMessageInfo,
+  InternalMessage,
+  SendMode,
+  toNano,
+  TupleSlice,
+  WalletContract,
 } from "ton";
-import {buildRegistryDataCell, Queries, Verifier} from "../packages/contracts/registry-contract/RegistryData";
+import {
+  buildRegistryDataCell,
+  Queries,
+  Verifier,
+} from "../packages/contracts/registry-contract/RegistryData";
 import BN from "bn.js";
-import {randomKeyPair} from "../packages/utils/randomKeyPair";
+import { randomKeyPair } from "../packages/utils/randomKeyPair";
+import { createHash } from "crypto";
 
 // return the init Cell of the contract storage (according to load_data() contract method)
 export function initData() {
-    return buildRegistryDataCell({
-        verifiers: new Map<BN, Verifier>()
-    }, 0)
+  return buildRegistryDataCell(
+    {
+      verifiers: new Map<BN, Verifier>(),
+    },
+    0
+  );
 }
 
 // return the op that should be sent to the contract on deployment, can be "null" to send an empty message
 export function initMessage() {
-    return null;
+  return null;
+}
+
+function sha256BN(name: string) {
+  return new BN(createHash("sha256").update(name).digest());
+}
+
+function ip2num(ip: string) {
+  let d = ip.split(".");
+  return ((+d[0] * 256 + +d[1]) * 256 + +d[2]) * 256 + +d[3];
 }
 
 // optional end-to-end sanity test for the actual on-chain contract to see it is actually working on-chain
-export async function postDeployTest(walletContract: WalletContract, secretKey: Buffer, contractAddress: Address) {
-    const call = await walletContract.client.callGetMethod(contractAddress, "get_verifiers_num");
-    let ts = new TupleSlice(call.stack);
-    console.log(`   # Getter 'get_verifiers_num' = ${ts.readNumber()}`);
-/*
-    let kp = await randomKeyPair()
-    let kp2 = await randomKeyPair()
-    let kp3 = await randomKeyPair()
+export async function postDeployTest(
+  walletContract: WalletContract,
+  secretKey: Buffer,
+  contractAddress: Address
+) {
+  const call = await walletContract.client.callGetMethod(
+    contractAddress,
+    "get_verifiers_num"
+  );
+  let ts = new TupleSlice(call.stack);
+  console.log(`   # Getter 'get_verifiers_num' = ${ts.readNumber()}`);
 
-    const seqno = await walletContract.getSeqNo();
-    const transfer = walletContract.createTransfer({
-        secretKey: secretKey,
-        seqno: seqno,
-        sendMode: SendMode.PAY_GAS_SEPARATLY + SendMode.IGNORE_ERRORS,
-        order: new InternalMessage({
-            to: contractAddress,
-            value: toNano(1),
-            bounce: false,
-            body: new CommonMessageInfo({
-              //  body: new CellMessage(Queries.removeVerifier({
-               //     id: new BN(717),
-             //   }))
-                body: new CellMessage(Queries.updateVerifier({
-                    id: new BN(77187),
-                    quorum: 2,
-                    endpoints: new Map<BN, number>([
-                        [new BN(kp.publicKey), 1],
-                        [new BN(kp2.publicKey), 2],
-                        [new BN(kp3.publicKey), 3]
-                    ])
-                }))
-            }),
-        }),
-    });
-    await walletContract.client.sendExternalMessage(walletContract, transfer);
+  const pubKey = [
+    148, 69, 177, 132, 85, 101, 80, 233, 160, 155, 14, 151, 13, 47, 168, 10,
+    213, 126, 6, 117, 246, 135, 231, 230, 73, 52, 59, 79, 177, 235, 35, 114,
+  ];
 
-    console.log(`   # Message sent`);
- */
+  const seqno = await walletContract.getSeqNo();
+  const transfer = walletContract.createTransfer({
+    secretKey: secretKey,
+    seqno: seqno,
+    sendMode: SendMode.PAY_GAS_SEPARATLY + SendMode.IGNORE_ERRORS,
+    order: new InternalMessage({
+      to: contractAddress,
+      value: toNano(1),
+      bounce: true,
+      body: new CommonMessageInfo({
+        //  body: new CellMessage(Queries.removeVerifier({
+        //     id: new BN(717),
+        //   }))
+        body: new CellMessage(
+          Queries.updateVerifier({
+            id: sha256BN("orbs.com"),
+            quorum: 1,
+            endpoints: new Map<BN, number>([[new BN(pubKey), 1]]),
+            name: "orbs.com",
+            marketingUrl: "https://orbs.com",
+          })
+        ),
+      }),
+    }),
+  });
+  await walletContract.client.sendExternalMessage(walletContract, transfer);
+
+  console.log(`   # Message sent`);
 }
